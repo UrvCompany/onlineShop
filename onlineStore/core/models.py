326f0ac2746cd2d8d8f6ac63464607d3a0810d12
mr_undercross <mrUndercross@gmail.com>
2025-08-11 10:43:50 +0300

    BReakpoint

--- onlineStore/core/models.py
+++ onlineStore/core/models.py
@@ -58,7 +58,7 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('main:product_detail',
+        return reverse('core:product_detail',
                        args=[self.slug])
 
     def sell_price(self):
