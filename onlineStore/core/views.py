61d8d2528941e0a5d0887327bcdb1a2deb566077
Urvantsev Konstantin <urvantsev@legaldoc.ai>
2025-10-19 06:09:10 +0300

    создал пагинацию

--- onlineStore/core/views.py
+++ onlineStore/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from  .models import Product, Category
 
 
@@ -40,18 +41,24 @@ def product_list(request, category_slug=None):
     :return:
     """
 
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 5)
+    current_page = paginator.page(int(page))
 
     if category_slug:
-        category = get_object_or_404(Category,
-                                     slug=category_slug)
-        products = products.filter(category=category)
+        category = get_object_or_404(Category, slug=category_slug)
+
+        paginator = Paginator(products.filter(category=category),5)
+        current_page = paginator.page(int(page))
 
     return render(request,
                   'core/product/list.html',
                   {'category': category,
                            'categories': categories,
-                           'products': products})
+                           'products': current_page,
+                           'slug_url': category_slug,
+                           })
 
